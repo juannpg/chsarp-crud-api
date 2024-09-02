03165b194be39f6f361cfe9d6ab531cdf754fee9
juannpg <juan.pasgut.workspace@gmail.com>
2024-09-03 01:23:46 +0200

    first commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+using Data;
+using Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace csharp_crud_api.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+  private readonly UserContext _context;
+
+  public UsersController(UserContext context)
+  {
+    _context = context;
+  }
+
+  [HttpGet("getUsers")]
+  public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+  {
+    return await _context.Users.ToListAsync();
+  }
+
+  [HttpGet("getUser")]
+  public async Task<ActionResult<User>> GetUserWithId(int id)
+  {
+    return await _context.Users.SingleOrDefaultAsync(user => user.Id == id);
+  }
+
+  [HttpPost("createUser")]
+  public async Task<ActionResult<User>> CreateUser(User user)
+  {
+    _context.Users.Add(user);
+    await _context.SaveChangesAsync();
+
+    return CreatedAtAction("GetUserWithId", new { id = user.Id }, user);
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+using Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var configuration = builder.Configuration;
+builder.Services.AddDbContext<UserContext>(options =>
+    options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseHttpsRedirection();
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
